c1e654d2904616090a0e7895a993bf820e04fc17
HMPerson1 <hmperson1+github@gmail.com>
2014-03-08 16:12:48 -0500

    GPL updates (Part 3)
    Changed name

--- src/hmperson1/apps/superdoughnuts/gui/GamePainter.scala
+++ src/hmperson1/apps/superdoughnuts/gui/GamePainter.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 hmperson1@gmail.com
+ * Copyright (C) 2014 HMPerson1 <hmperson1@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
